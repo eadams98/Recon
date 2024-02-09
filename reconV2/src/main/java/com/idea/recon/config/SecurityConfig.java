55c8db498fc99599aa97d2fc50792a49a221ad46
eadams98 <eric051598@gmail.com>
2024-02-08 19:33:04 -0600

    Update SecurityConfig.java
    adding netlify domain to cors security list

--- reconV2/src/main/java/com/idea/recon/config/SecurityConfig.java
+++ reconV2/src/main/java/com/idea/recon/config/SecurityConfig.java
@@ -165,6 +165,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://verdant-mermaid-5bfec4.netlify.app"));
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
