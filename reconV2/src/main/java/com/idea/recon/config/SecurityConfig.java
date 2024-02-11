3a14d278ed41ba9abe81568a17478990e7467f16
eadams98 <eric051598@gmail.com>
2024-02-10 19:31:46 -0600

    Update SecurityConfig.java
    Allow options (for netlify)

--- reconV2/src/main/java/com/idea/recon/config/SecurityConfig.java
+++ reconV2/src/main/java/com/idea/recon/config/SecurityConfig.java
@@ -169,7 +169,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
